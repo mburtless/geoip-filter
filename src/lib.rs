ecef78359fa17f6410e14b8bbbf8afaa95f750f4
Matthew Burtless <mburtless@threepnet.com>
2022-08-02 14:09:59 -0400

    base rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,17 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use log::trace;
+use proxy_wasm::traits::*;
+use proxy_wasm::types::*;
+
+struct GeoIPFilter;
+
+impl Context for GeoIPFilter{}
+
+impl HttpContext for GeoIPFilter {
+    fn on_http_request_headers(&mut self, _: usize, _: bool) -> Action {
+        for (name, value) in &self.get_http_request_headers() {
+            trace!("In WASM -> {}: {}", name, value);
+        }
+
+        Action::Pause
     }
 }
